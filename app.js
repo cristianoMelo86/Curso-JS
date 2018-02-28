184a448d865ac15a2dfdacfab283eafd163e2f98
Cristiano <flashbass19@gmail.com>
2018-02-28 14:51:50 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+(function(){
+
+	var trs = document.querySelectorAll('.table tr');
+
+	var i = 0;
+	 while (trs[i]) {
+	 	var linhaAluno = trs[i];
+	 	console.log(linhaAluno);
+	 	i++;
+	 }
+
+
+
+
+})();
