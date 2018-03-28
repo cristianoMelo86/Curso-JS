b9690d5c219d1a993b89707aaacd818272ad45a9
Cristiano <flashbass19@gmail.com>
2018-03-28 15:35:02 -0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,49 @@
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
-
-gulp.task('default', function() {
-  console.log("Ola mundo");
-});
-
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 
  
-gulp.task('minify', function() {
-  return gulp.src('*.html')
+gulp.task('htmlmin', function() {
+  return gulp.src('./src/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./dist'));
+
+
 });
+
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/sass/*.scss', ['sass']);
+});
+
+gulp.task('html:watch', function () {
+  gulp.watch('./src/*.html', ['htmlmin']);
+})
+
+
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+
+
+
+gulp.task('serve', function() {
+
+    browserSync.init({
+        server: "./dist"
+    });
+
+    gulp.watch("./src/*.html", ['html:watch']).on('change', browserSync.reload);
+
+
+});
+
+
+gulp.task('default', ['serve']);
 
 
